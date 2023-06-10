00c49ddc6c23d66b4f8e97442615d8bb775557e7
Sergey <zabelich19@gmail.com>
2023-06-10 13:00:25 +0300

    создал файл приветствия любимчика

---
+++ example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Ввдите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "сергей")
+{
+    Console.WriteLine("Чего изволите мой Господин! ");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
